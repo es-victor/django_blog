337fd69d58a247ecf99542a85baed617195f1491
Victor Ephraim <ephraimvictor@gmail.com>
2021-02-12 08:20:56 +0300

    create, update, delete, authorize

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -12,5 +13,8 @@ class Post(models.Model):
     def __str__(self):
         return self.title 
 
-    class Meta:
-        ordering = ('-date_posted',)
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
+    
+    # class Meta:
+    #     ordering = ('-date_posted',)
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,63 @@
 from django.shortcuts import render
 from datetime import time, date, datetime
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView)
+
+# def home(request):
+#     # dictionary to pass data(posts)
+#     context = {
+#         'posts': Post.objects.all(),
+#         'title': 'Home Page',
+#         'home': 'active',
+#     }
+#     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 
-def home(request):
-    # dictionary to pass data(posts)
-    context = {
-        'posts': Post.objects.all(),
-        'title': 'Home Page',
-        'home': 'active',
-    }
+class PostDetailView(DetailView):
+    model = Post
+# created a template with name = post_detail.html to match the default naming convetion so to remove the need for template_name as before(above)
 
-    return render(request, 'blog/home.html', context)
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView( LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False 
+
+
+
+class PostDeleteView( LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    fields = ['title', 'content']
+    success_url = '/'
+ 
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False 
 
 
 def about(request):
@@ -19,5 +65,4 @@ def about(request):
         'title': 'About Page',
         'about': 'active',
     }
-
     return render(request, 'blog/about.html', context)
