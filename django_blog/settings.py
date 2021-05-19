bea6a9cea02b224d573da8a5e7bf7e6f6adaf4ec
Victor Ephraim <ephraimvictor@gmail.com>
2021-05-19 17:19:41 +0300

    server error

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY =  "w+k^o%ol!_tzx8ll14xd8!_+90!nok-^pl=e8wra3b%m2iqn7e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-DEBUG = "True"
+DEBUG = "False"
 ALLOWED_HOSTS = [
 
     'victor-django-blog.herokuapp.com', '127.0.0.1'
