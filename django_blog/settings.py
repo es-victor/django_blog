0a0b441168a0bbaa50d094077f09ed0435fba0e5
Victor Ephraim <ephraimvictor@gmail.com>
2021-05-19 16:38:36 +0300

    KEY

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w+k^o%ol!_tzx8ll14xd8!_+90!nok-^pl=e8wra3b%m2iqn7e'
+# secret w+k^o%ol!_tzx8ll14xd8!_+90!nok-^pl=e8wra3b%m2iqn7e
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'w+k^o%ol!_tzx8ll14xd8!_+90!nok-^pl=e8wra3b%m2iqn7e')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '')!='False'
